d0fe166e2b67e08154156b7480c852fd0faac1bf
Serhan Gyuler <serhan.gyuler@hotmail.com>
2024-09-30 13:38:05 +0200

    Add project files.

---
+++ WordGuessingGameGroup/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WordGuessingGameGroup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
